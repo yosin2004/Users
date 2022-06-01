22dc1779454af39c4f238a0e5a08a597479aa3a2
Saidov Yosin <saidovyosin2004@gmail.com>
2022-06-01 17:53:24 +0300

    commit deleted

--- index.js
+++ index.js
@@ -1,7 +1,42 @@
+var modal = document.getElementById("myModal")
+var btn = document.getElementById("myNtn")
+var send = document.querySelector("#send")
+var span = document.getElementsByClassName("close")[0];
+
+
+
+const deletedUser = async (id) => {
+    try{
+        const response = await fetch(`https://62726b39c455a64564c0d314.mockapi.io/users/${id}`,
+        {
+            method: "DELETE",
+        }
+        ) 
+        const data = await response.json()
+        alert(`${data.message}`)
+        window.location.reload()
+    }  catch (error){
+     console.error(error)
+    }
+}
+const postUser = async () => {
+    try {
+        const response = await fetch("https://62726b39c455a64564c0d314.mockapi.io/users",
+        {
+            method: "POST",
+            body: JSON.stringify(user),
+        }
+        )
+        const data = await response.json()
+    } catch (error) {
+        console.error(error)
+    }
+}
+
 let tbody = document.querySelector(".tbody");
-const getUsers = async () => {
+const getUsers = async (id) => {
   try {
-    const response = await fetch("https://62726b39c455a64564c0d314.mockapi.io/users");
+    const response = await fetch(`https://62726b39c455a64564c0d314.mockapi.io/users`);
     const data = await response.json();
     console.log(data);
     data.forEach((elem) => {
@@ -18,13 +53,20 @@ const getUsers = async () => {
         email.innerHTML = elem.email
         let phone = document.createElement("td")
         phone.innerHTML = elem.phone
-        
+        let deleted = document.createElement("button")
+        deleted.innerHTML = "deleted"
+        deleted.onclick = () => {
+
+            deletedUser(elem.id, getUsers)
+            
+        }
         tr.appendChild(id)
         tr.appendChild(createdAt)
         tr.appendChild(name)
         tr.appendChild(age)
         tr.appendChild(email)
         tr.appendChild(phone)
+        tr.appendChild(deleted)
         tbody.appendChild(tr)
     })
   } catch (error) {
